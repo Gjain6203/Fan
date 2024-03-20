cac1396697cfe29822fb55f0a723d8a48c2e4082
Gaurav Jain <gjain6203@gmail.com>
2024-03-20 21:34:08 +0530

    add some files

---
+++ js/fan.js
@@ -0,0 +1,13 @@
+
+let a = document.getElementById("img");
+
+function myfunon()
+{a.style.animationDuration = 3+"s";}
+function myfunoff()
+{a.style.animationDuration = 0+"s";}
+function myfun01()
+{a.style.animationDuration = 1+"s"}
+function myfun02()
+{a.style.animationDuration = 0.5+"s"}
+function myfun03()
+{a.style.animationDuration = 0.1+"s"}
